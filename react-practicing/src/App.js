c42451c7210e4457d66cc5e65d09091c0ec2631a
Madhawa-Tharindu <madhawath@gmail.com>
2022-07-06 17:33:57 +0530

    Setup environment

---
+++ react-practicing/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Namelist from './components/NameList/NameLIst';
+
+function App() {
+  return (
+    <div>
+      <Namelist />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-practicing/src/components/NameList/NameLIst.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Namelist(){
+    return (
+        <React.Fragment>
+    <h1> Name List </h1>        
+ <li>
+    <ul>Madhawa Tharindu - Software Engineer</ul>
+    <ul>Madhawa Tharindu - Software Engineer</ul>
+    <ul>Madhawa Tharindu - Software Engineer</ul>
+  </li>
+        </React.Fragment>
+    )
+  
+}
+
+export default Namelist;
