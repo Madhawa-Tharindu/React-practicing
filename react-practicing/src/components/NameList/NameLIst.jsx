92bb0a5fd23c302fdcc3e18a8d6cccffa8cc1167
Madhawa-Tharindu <madhawath@gmail.com>
2022-07-12 07:10:23 +0530

    used useState

--- react-practicing/src/components/NameList/NameLIst.jsx
+++ react-practicing/src/components/NameList/NameLIst.jsx
@@ -1,52 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import NameListItem from './NameListItem';
 
 function Namelist(){
-
-    const nameList = [{
-        "id": 2,
-        "name": {
-        "title": "Miss",
-        "first": "Jennie",
-        "last": "Nichols"
-      },
-      "location": {
-        "city": "Billings",
-      },
-      "email": "jennie.nichols@example.com",
-      "dob": {
-        "date": "1992-03-08T15:13:16.688Z",
-        "age": 30
-      },
-      "picture": {
-        //"large": "https://randomuser.me/api/portraits/men/75.jpg",
-        "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
-        //"thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
-      },
-    },
-    {
-    "id": 1,    
+  const [nameList, setNameList] = useState([{
+    "id": 2,
     "name": {
-        "title": "Miss",
-        "first": "Jennie",
-        "last": "Nichols"
-      },
-      "location": {
-        "city": "Billings",
-      },
-      "email": "jennie.nichols@example.com", 
-      "dob": {
-        "date": "1992-03-08T15:13:16.688Z",
-        "age": 30
-      },
-      "picture": {
-        //"large": "https://randomuser.me/api/portraits/men/75.jpg",
-        "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
-        //"thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
-      }, 
-    }
-];
+    "title": "Miss",
+    "first": "Jennie",
+    "last": "Nichols"
+  },
+  "location": {
+    "city": "Billings",
+  },
+  "email": "jennie.nichols@example.com",
+  "dob": {
+    "date": "1992-03-08T15:13:16.688Z",
+    "age": 30
+  },
+  "picture": {
+    //"large": "https://randomuser.me/api/portraits/men/75.jpg",
+    "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+    //"thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+  },
+},
+{
+"id": 1,    
+"name": {
+    "title": "Miss",
+    "first": "Jennie",
+    "last": "Nichols"
+  },
+  "location": {
+    "city": "Billings",
+  },
+  "email": "jennie.nichols@example.com", 
+  "dob": {
+    "date": "1992-03-08T15:13:16.688Z",
+    "age": 30
+  },
+  "picture": {
+    //"large": "https://randomuser.me/api/portraits/men/75.jpg",
+    "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+    //"thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+  }, 
+}
+]);
+
+
 
 const nameListComponent = () => {
     return nameList.map(aName => {
@@ -61,11 +62,42 @@ const nameListComponent = () => {
               );
         });
 };
+
+const addUserHandler = () => {
+  const newUser = {
+    
+      "id": 1,    
+      "name": {
+        "title":"Miss",
+        "first":"Pallavi",
+        "last":"Namnaik"
+        },
+        "location": {
+          "city": "Billings",
+        },
+        "email": "jennie.nichols@example.com", 
+        "dob": {
+          "date": "1992-03-08T15:13:16.688Z",
+          "age": 30
+        },
+        "picture": {
+          //"large": "https://randomuser.me/api/portraits/men/75.jpg",
+          "medium":"https://randomuser.me/api/portraits/med/women/37.jpg"
+          //"thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+        },
+
+  };
+  //setNameList((nameList) => nameList.concat(newUser));
+  //spread operater
+
+  setNameList((nameList) => [...nameList, newUser]);
+};
     return (
         <React.Fragment>
         
         <div className="container-fluid mt-4">
         <ul className="list-group">
+          <button className='btn btn-primary mb-2' onClick={addUserHandler}>Add Name</button>
     {nameListComponent()}
       
   </ul>
